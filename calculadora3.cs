696e4d07fd13f037d68799ef908940e23274eecc
amelia588 <1473967@senati.pe>
2024-03-07 13:56:52 -0500

    actualizando

--- calculadora3.cs
+++ calculadora3.cs
@@ -25,61 +25,61 @@ namespace calculadora3
         private void btn0_Click(object sender, EventArgs e)
         {
             //numero 0
-            tbDisplay.Text = "0";
+            tbDisplay.Text = tbDisplay.Text + "0";
         }
 
         private void btn1_Click(object sender, EventArgs e)
         {
             //numero 1
-            tbDisplay.Text = "1";
+            tbDisplay.Text = tbDisplay.Text + "1";
         }
 
         private void btn2_Click(object sender, EventArgs e)
         {
             //numero 2
-            tbDisplay.Text = "2";
+            tbDisplay.Text = tbDisplay.Text + "2";
         }
 
         private void btn3_Click(object sender, EventArgs e)
         {
             //numero 3
-            tbDisplay.Text = "3";
+            tbDisplay.Text = tbDisplay.Text + "3";
         }
 
         private void btn4_Click(object sender, EventArgs e)
         {
             //numero 4
-            tbDisplay.Text = "4";
+            tbDisplay.Text = tbDisplay.Text + "4";
         }
 
         private void btn5_Click(object sender, EventArgs e)
         {
             //numero 5
-            tbDisplay.Text = "5";
+            tbDisplay.Text = tbDisplay.Text + "5";
         }
 
         private void btn6_Click(object sender, EventArgs e)
         {
             //numero 6
-            tbDisplay.Text = "6";
+            tbDisplay.Text = tbDisplay.Text + "6";
         }
 
         private void btn7_Click(object sender, EventArgs e)
         {
             //numero 7
-            tbDisplay.Text = "7";
+            tbDisplay.Text = tbDisplay.Text + "7";
         }
 
         private void btn8_Click(object sender, EventArgs e)
         {
             //numero 8
-            tbDisplay.Text = "8";
+            tbDisplay.Text = tbDisplay.Text + "8";
         }
 
         private void btn9_Click(object sender, EventArgs e)
         {
             //numero 9
-            tbDisplay.Text = "9";
+            tbDisplay.Text = tbDisplay.Text + "9";
         }
     }
 }
